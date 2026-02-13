9948495e9bf7543dc50bff7eb2c33f178dbe84ce
Sebastian <sebbs21@gmail.com>
2026-02-13 16:56:45 -0600

    Cleanup: remove redundant data files - single source of truth (1858 matches)

--- src/services/vercelDataService.js
+++ src/services/vercelDataService.js
@@ -1,28 +1,13 @@
 // Service para cargar datos históricos de Sporting Cristal
 import completoData from '../data/historico_completo_sc.json';
-import incaData from '../data/historico_inca_sc.json';
-import conmebolData from '../data/historico_conmebol_sc.json';
 
 class VercelDataService {
-  // Los datos se cargan directamente desde archivos JSON locales incluidos en el bundle
-  // Sin async para evitar delay de carga
-
-  fetchConmebolData() {
-    return conmebolData;
-  }
-
-  fetchIncaData() {
-    return incaData;
-  }
-
   fetchCompleteData() {
     return completoData;
   }
 
   fetchAllData() {
     return {
-      conmebol: conmebolData,
-      inca: incaData,
       completo: completoData
     };
   }
